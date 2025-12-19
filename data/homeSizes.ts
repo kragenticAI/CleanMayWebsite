9822510de987a6b7214816663b62f10d8d1d0735
Baljinder Singh <baljinder.singh@kragentic.com>
2025-12-19 12:17:21 +0530

    ui updates

---
+++ data/homeSizes.ts
@@ -0,0 +1,28 @@
+export const homeSizesData = {
+  regular: [
+    { id: 1, title: "1 bd / 1 ba", subtitle: "< 1000 sq.ft.", price: 258 },
+    { id: 2, title: "2 bd / 2 ba", subtitle: "> 1000 sq.ft.", price: 278 },
+    { id: 3, title: "3 bd / 2 ba", subtitle: "< 1800 sq.ft.", price: 298 },
+    { id: 4, title: "4 bd / 2 ba", subtitle: "> 1800 sq.ft.", price: 318 },
+    { id: 5, title: "5 bd / 2 ba", subtitle: "< 2250 sq.ft.", price: 338 },
+    { id: 6, title: "6 bd / 2 ba", subtitle: "> 2250 sq.ft.", price: 378 },
+  ],
+
+  deep: [
+    { id: 1, title: "1 bd / 1 ba", subtitle: "< 1000 sq.ft.", price: 298 },
+    { id: 2, title: "2 bd / 2 ba", subtitle: "> 1000 sq.ft.", price: 318 },
+    { id: 3, title: "3 bd / 2 ba", subtitle: "< 1800 sq.ft.", price: 348 },
+    { id: 4, title: "4 bd / 2 ba", subtitle: "> 1800 sq.ft.", price: 378 },
+    { id: 5, title: "5 bd / 2 ba", subtitle: "< 2250 sq.ft.", price: 398 },
+    { id: 6, title: "6 bd / 2 ba", subtitle: "> 2250 sq.ft.", price: 428 },
+  ],
+
+  moving: [
+    { id: 1, title: "Studio / 1 bd", subtitle: "< 800 sq.ft.", price: 320 },
+    { id: 2, title: "2 bd / 1 ba", subtitle: "< 1200 sq.ft.", price: 360 },
+    { id: 3, title: "3 bd / 2 ba", subtitle: "< 1600 sq.ft.", price: 400 },
+    { id: 4, title: "4 bd / 2 ba", subtitle: "> 1800 sq.ft.", price: 440 },
+      { id: 5, title: "5 bd / 2 ba", subtitle: "< 2250 sq.ft.", price: 500 },
+    { id: 6, title: "6 bd / 2 ba", subtitle: "> 2250 sq.ft.", price: 460},
+  ],
+};
